b4bdb6fc8cf49098cac17c98bfdbf7a50bde52ea
LeeJinWoo <jw.lee@lulu-lab.com>
2024-05-29 09:17:34 +0900

    Initial commit

---
+++ src/main/java/com/example/behavior_driven_development/BehaviorDrivenDevelopmentApplication.java
@@ -0,0 +1,13 @@
+package com.example.behavior_driven_development;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BehaviorDrivenDevelopmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BehaviorDrivenDevelopmentApplication.class, args);
+	}
+
+}
