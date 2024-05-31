309d9ea2e4727ad4456e2565542dfb497f5c98d8
LeeJinWoo <jw.lee@lulu-lab.com>
2024-05-31 15:08:33 +0900

    embedded db -> test container 변경 작업중

--- src/test/java/com/example/behavior_driven_development/base/BaseIntegrationTest.java
+++ src/test/java/com/example/behavior_driven_development/base/BaseIntegrationTest.java
@@ -1,16 +1,24 @@
 package com.example.behavior_driven_development.base;
 
+import com.example.behavior_driven_development.config.TestContainerConfig;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Disabled;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.transaction.annotation.Transactional;
+import org.testcontainers.containers.MySQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
 @SpringBootTest
+@ContextConfiguration(classes = {TestContainerConfig.class})
 @Disabled // 클래스 또는 클래스에 정의된 메소드를 실행하지 않기 위해 선언
 @AutoConfigureMockMvc // SpringBootTest환경에서 MockMvc를 사용하기 위해 선언
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 @Transactional // 테스트 코드에서 Transaction이 발생할 경우 해당 데이터를 롤백하기 위해 선언
 public class BaseIntegrationTest extends BaseTest {
     @Autowired
@@ -18,4 +26,7 @@ public class BaseIntegrationTest extends BaseTest {
 
     @Autowired
     protected ObjectMapper objectMapper;
+
+    @Autowired
+    private MySQLContainer<?> mySQLContainer;
 }
