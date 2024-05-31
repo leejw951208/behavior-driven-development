2885f68dc0988627b70e59d7a76bafbf2067fa08
LeeJinWoo <jw.lee@lulu-lab.com>
2024-05-31 15:19:54 +0900

    embedded db -> test container 변경 작업중

--- src/test/java/com/example/behavior_driven_development/base/BaseIntegrationTest.java
+++ src/test/java/com/example/behavior_driven_development/base/BaseIntegrationTest.java
@@ -1,6 +1,7 @@
 package com.example.behavior_driven_development.base;
 
 import com.example.behavior_driven_development.config.TestContainerConfig;
+import com.example.behavior_driven_development.config.TestObjectMapperConfig;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Disabled;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,7 +16,7 @@ import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
 @SpringBootTest
-@ContextConfiguration(classes = {TestContainerConfig.class})
+/*@ContextConfiguration(classes = {TestContainerConfig.class, TestObjectMapperConfig.class})*/
 @Disabled // 클래스 또는 클래스에 정의된 메소드를 실행하지 않기 위해 선언
 @AutoConfigureMockMvc // SpringBootTest환경에서 MockMvc를 사용하기 위해 선언
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
@@ -26,7 +27,4 @@ public class BaseIntegrationTest extends BaseTest {
 
     @Autowired
     protected ObjectMapper objectMapper;
-
-    @Autowired
-    private MySQLContainer<?> mySQLContainer;
 }
